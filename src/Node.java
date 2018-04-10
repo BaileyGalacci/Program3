282d12073cd581c16ee31c81961648469e10a7b0
Bailey Galacci <bailey0522@gmail.com>
2018-04-10 14:09:58 -0600

    fixing queue issues

--- src/Node.java
+++ src/Node.java
@@ -69,6 +69,8 @@ public class Node {
     
     
     // methods
+    // @param capacity
+    // 
     public void setEdge(int edgeN, Node inDest, int inWeight){
         this.edge[edgeN] = new Edge(inDest, inWeight);
         return;
@@ -85,7 +87,8 @@ public class Node {
         private Edge[] heap; 
         private int heapSize, capacity;
     
-        /** Constructor **/
+        /** Constructor
+         * @param capacity **/
         public PriorityQueue(int capacity)
         {    
             this.capacity = capacity + 1;
@@ -138,9 +141,10 @@ public class Node {
                 return null;
             }
     
-            item = heap[1];
+            item = heap[heapSize];
             temp = heap[heapSize--];
     
+    /**
             parent = 1;
             child = 2;
             while (child <= heapSize)
@@ -155,7 +159,7 @@ public class Node {
                 child *= 2;
             }
             heap[parent] = temp;
-    
+    **/
             return item;
         }
     }
