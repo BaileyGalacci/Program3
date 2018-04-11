ff69d17b9e480e75d1df4599980c75cfab8f8742
Bailey Galacci <bailey0522@gmail.com>
2018-04-10 21:43:50 -0600

    almost done with Dijkstra

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -17,7 +17,8 @@ import java.lang.String;
  */
 /**
  *
- * @author g96t776 Date: 4/11/18 Overview: WeightedGraph is the main class of
+ * authors: Edward Galacci, Hans Mazur
+ * Date: 4/10/18 Overview: WeightedGraph is the main class of
  * the project Takes a file input and converts it into a weighted graph that can
  * later be converted into a minimum spanning tree Contains the String array
  */
@@ -26,11 +27,13 @@ public class WeightedGraph {
     public Node[] nodeList;
     Tree primTree, kruskalTree;
     Tree[] Forest;
+    int[][] dist;
 
     public WeightedGraph() {
         nodeList = new Node[6];
         primTree = new Tree();
         kruskalTree = new Tree();
+        dist = new int[6][6];
         Forest = new Tree[5];
         for (int j = 0; j < 5; j++) {
             Forest[j] = new Tree();
@@ -58,6 +61,15 @@ public class WeightedGraph {
         nodeList[location] = new Node(inName);
     }
 
+    // print out distance table
+    public void printTable(int[][] data, int i){
+        for(int j=0; j<i; j++){
+                for(int k=0; k < i; k++){
+                   
+                }
+            }
+    }
+    
     public static void main(String[] args) throws IOException {
 
         WeightedGraph theGraph = new WeightedGraph();
@@ -106,7 +118,7 @@ public class WeightedGraph {
 
             System.out.println("We did it?\n");
 
-            // Prim's algorithm
+            // Prim's algorithm     //////////////////////////////////////////////////////
             // start at the first vertex
             int[] D = new int[i];
             Node.Edge[] edgeTemp;
@@ -170,8 +182,8 @@ public class WeightedGraph {
                 System.out.printf("\n%c => %c",startNode,endNode);
             }
             System.out.println("");
-
-            // KRUSKALS ALGORITHM
+/**
+            // KRUSKALS ALGORITHM       ////////////////////////////////////////////////
             //unmarks everything and populates the forest
             for (j = 0; j < i; j++) {
                 theGraph.nodeList[j].setIsMarked(false);
@@ -180,7 +192,7 @@ public class WeightedGraph {
             
             
             //creates edge list
-            Node.Edge[] edges = new Node.Edge[20];
+            Node.Edge[] edges = new Node.Edge[21];
             for (j = 0; j < 5; j++) {
                 for (int k = 0; k < 5; k++) {
                     if (j==k) {
@@ -201,12 +213,37 @@ public class WeightedGraph {
                         w = edges[j].weight;
                         e = edges[j]; 
                     }
+                } 
+            }
+**/            
+            // Dijkstra's Algorithm         /////////////////////////////////////
+            
+            for (j = 0; j < i; j++) {   // unmark all the nodes again
+                theGraph.nodeList[j].setIsMarked(false);
+                for (int k=0; k< i; k++){   // populate all with standard values, or inf if they are not present
+                    theGraph.dist[j][k] = Integer.MAX_VALUE;
+                    try{
+                        theGraph.dist[j][k] = theGraph.nodeList[j].getEdge(k).weight;
+                    } catch (NullPointerException y) {
+                        System.err.format("NullPointerException:asfasdf %s%n", y);
+                    }
                 }
-                
                 
             }
             
+            // Start at node 0, repeat until no more nodes are left ('i' will track this)
+            for(j=0; j<i; j++){
+                for(int k=0; k < i; k++){
+                    for(int l=0; l<i; l++){
+                        if (theGraph.dist[i][j] > theGraph.dist[i][k] + theGraph.dist[k][j]){
+                            theGraph.dist[i][j] = theGraph.dist[i][k] + theGraph.dist[k][j];
+                        }
+                    }
+                }
+            }
+                
             
+           
 
         } catch (IOException x) {
             System.err.format("IOException: %s%n", x);
